038480d24214ce574a46f1b99224bf45cc424ae1
Deyruby <128061593+Deyruby@users.noreply.github.com>
2024-04-21 03:19:35 +0000

    avances

---
+++ src/js/component/cpu-board.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Cpuboard = () => {
+
+	const tags = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+	return (
+		<>
+			
+			<div className="board">
+			<div className="vertical-tags">
+					{
+							tags.map((element, index) => <th className="col-tags" key={index}>{element}</th>)
+						}
+					</div>
+				<table>
+					<thead className="table-head">
+						{
+							tags.map((element, index) => <th className="row-tags" key={index}>{element}</th>)
+						}
+					</thead>
+					<tbody>
+						{tags.map((element)=>{
+						return (
+							<tr>
+								{
+									tags.map((element, index) => <td className="rows" key={index}></td>)
+								}
+							</tr>)
+							})}
+					</tbody>
+				</table>
+			</div>
+		</>
+	);
+
+};
+
+export default Cpuboard;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,20 +1,28 @@
 import React from "react";
-
+import Userboard from "./user-board";
+import Cpuboard from "./cpu-board";
 
 
 
 const Home = () => {
+
+
+
+
 	return (
-		<div className="text-center">
+		<>
 			<h1 className="game-tittle">Battleship Game</h1>
-			<div className="board">
-				<div className="player-user">
-					{}
-				</div>
-				<div className="player-pc"></div>
+			<div className="tittles">
+			<h3 className="user-tittle">User</h3>
+			<h3 className="cpu-tittle">CPU</h3>
 			</div>
-		</div>
+			<div className="total-board">
+			<Userboard/>
+			<Cpuboard/>
+			</div>
+		</>
 	);
+
 };
 
 export default Home;
